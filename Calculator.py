6b062db3aff406e16a38f941dc0950d00680321e
Bhaskar pati <patibhaskar2000@gmail.com>
2025-11-05 15:22:32 +0530

    initial commit

---
+++ Calculator.py
@@ -0,0 +1,18 @@
+print("Calculator")
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+op = input("Enter operator (+, -, *, /, % , //): ")
+
+if op == '+':
+    print("Result =", num1 + num2)
+elif op == '-':
+    print("Result =", num1 - num2)
+elif op == '*':
+    print("Result =", num1 * num2)
+elif op == '/':
+    print("Result =", num1 / num2)
+elif op == '%':
+    print("Result =", num1 % num2)
+elif op == '//':
+    print("Result =", num1 // num2)
+
---
+++ Python Vote Task.py
@@ -0,0 +1,16 @@
+
+num = float(input("Enter a number: "))
+
+if num > 0:
+    print("The number is Positive.")
+elif num < 0:
+    print("The number is Negative.")
+else:
+    print("The number is Zero.")
+
+age = int(input("\nEnter your age: "))
+
+if age >= 18:
+    print("You are eligible to vote ")
+else:
+    print("You are not eligible to vote ")
